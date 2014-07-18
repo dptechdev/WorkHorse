45d96776c4a1c1fb2c326bd22480c7b7d7704ec2
dptechdev <dpauleytechnology@gmail.com>
2014-07-18 07:57:39 -0500

    Edit
    Edit

---
+++ WorkHorse.Domain/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkHorse.Domain
+{
+    
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime StartDate { get; set; }
+        public Department Department { get; set; }
+    }
+}
